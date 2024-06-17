7ed452f3cebfca03dc5caedf1d0729d9740265b2
Tiago Gonçalves Cesar <tgcprojetos@hotmail.com>
2024-06-17 07:28:42 -0300

    Projeto-Curso-javascript-da-Udemy

---
+++ Seção2-Java Script Basico para inicante/aula 21/script.js
@@ -0,0 +1,14 @@
+//Aqui resultado vai aparecer como NUMBER
+let num1 = prompt('Digite um número:');
+console.log('Primeiro número:',num1);
+let num2 = prompt('Digite outro número:');
+console.log('Segundo número:',num2);
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert(`A soma de num1 e num2 é:' ${num1 + num2}`); // aqui mostra a soma de num1 e num2
+console.log('num1 é do tipo',typeof num1,'e num2 é do tipo', typeof num2);
+
+
+
+
---
+++ Seção2-Java Script Basico para inicante/aula 21/script2.js
@@ -0,0 +1,8 @@
+//Aqui resultado vai aparecer como string
+let num3 = prompt('Digite um número:');
+console.log('Primeiro número:',num3);
+let num4 = prompt('Digite outro número:');
+console.log('Segundo número:',num4);
+const resultado1 = num3 + num4;
+ alert('A soma de num3 e num4 é: ' +resultado1);// aqui não aparece a soma de num3 + num4, mostra os dois números digitados
+console.log('num1 é do tipo',typeof num1,'e num2 é do tipo', typeof num2)
