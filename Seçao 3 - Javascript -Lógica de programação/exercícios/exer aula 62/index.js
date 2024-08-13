33346e96fa47fcc698d1f7ac220ae9a206cc4405
Tiago Gonçalves Cesar <tgcprojetos@hotmail.com>
2024-08-13 07:36:37 -0300

    Projeto-Curso-javascript-da-Udemy

--- Seçao 3 - Javascript -Lógica de programação/exercícios/exer aula 62/index.js
+++ Seçao 3 - Javascript -Lógica de programação/exercícios/exer aula 62/index.js
@@ -4,7 +4,7 @@
 // Número divísivel por 5 = Buzz
 // Número divísivel por 3 e 5 = FizzBuzz
 // Número NÃO divísivel por 3 e 5 = Retorna o próprio número
-//  Checar se o número é realmente número
+// Checar se o número é realmente número
 // Use a Função com números de 0 a 100
 
 function fizzBuzz(num) {
